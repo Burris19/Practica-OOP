5140de0ba441b965abd160ac4a8dfc2735245e79
Julian <julian@autoweb.com>
2016-09-09 17:43:40 +0000

    [add] first practice

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Person {
+  var $firstName;
+  var $lastName;
+
+  function __construct($firstName, $lastName) {
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+
+  }
+
+  function fullName() {
+    return $this->firstName . ' ' . $this->lastName
+  }
+}
+
+
+$person1 = new Person;
+$Person2 = new Person;
+
+echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
