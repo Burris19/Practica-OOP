50cf53ce3710f51481f5637c1c2df3816cef0b5c
Julian <julian@autoweb.com>
2016-09-16 17:00:52 +0000

    [add] interfaces y polimorfismo

--- herencia.php
+++ herencia.php
@@ -51,22 +51,46 @@ abstract class Unit {
     public function die()
     {
         show("{$this->name} muere");
+
+        exit();
     }
 }
 
 class Soldier extends Unit
 {
     protected $damage = 40;
+    protected $armor;
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+    }
+
+    public function setArmor(Armor $armor = null)
+    {
+        $this->armor = $armor;
+    }
 
     public function attack(Unit $ooponet)
     {
-        show("{$this->name} corta a {$opponent->getName()} en dos");
+        show("{$this->name} ataco con la espada a  {$opponent->getName()}");
         $opponent->takeDamage($this->damage);
     }
 
     public function takeDamage($damage)
     {
-        return parent::takeDamage($damage / 2);
+        $damage = $this->absorbDamage($damage);
+
+        return parent::takeDamage($damage);
+    }
+
+    public function absorbDamage($damage)
+    {
+        if ($this->armor) {
+            $damage = $this->armor->absorbDamage($damage);
+        }
+
+        return $damage;
     }
 
 }
@@ -82,7 +106,6 @@ class Archer extends Unit{
         $opponent->takeDamage($this->damage);
 
     }
-
     public function takeDamage($damage)
     {
         if (rand(0,1)) {
@@ -90,14 +113,40 @@ class Archer extends Unit{
         }
 
     }
-
-
 }
 
+interface Armor
+{
+    public function absorbDamage($damage);
+}
+
+
+class BronzeArmor implements Armor
+{
+    public function absorbDamage($damage)
+    {
+        return $damage / 2;
+    }
+}
+
+class CursedArmor implements Armor
+{
+    public function absorbDamage($damage)
+    {
+        return $damage * 2;
+    }
+}
+
+
+
+$armor = new BronzeArmor();
 $ram = new Soldier('Ram');
 
 $silence = new Unit('Silence');
-$silence->move('El norte');
+// $silence->move('El norte');
+
 $silence->attack($ram);
+
+$ram->setArmor($armor)
 
 $ram->attack($silence);
